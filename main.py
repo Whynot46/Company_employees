aaa059e21072761a8574808ff42cc59380e73755
Aleksey_Pahalev <apahalev@gmail.com>
2023-06-19 15:24:42 +0400

    update Main_window

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import config
 from kivymd.app import MDApp
 from kivy.core.window import Window
 from kivy.uix.screenmanager import ScreenManager, Screen
@@ -5,11 +6,8 @@ from kivy.core.window import Window
 from kivy.metrics import dp, sp
 from kivy.uix.popup import Popup
 from kivymd.uix.dialog.dialog import MDDialog
-from kivy.config import Config
 from kivymd.uix.datatables.datatables import MDDataTable
 
-Window.size = (sp(700), sp(310))
-Config.set('graphics', 'resizable', False)
 
 class Main_Screen(Screen):
     def add_people(self): pass
@@ -22,22 +20,31 @@ class Data_base(Screen):
 
 class Staff_info(Screen):
     def show_info(self):
-        self.dialog = MDDialog(text='Информация о сотруднике')
+        self.dialog = MDDialog(title='Информация о сотруднике',
+                               text='Очень важная информация')
         self.dialog.open()
     
 
-
 class Company_employees(MDApp):
     def build(self):
-        global SM
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "Blue"
         self.load_kv('company_employees.kv')
-        SM = ScreenManager()
-        SM.add_widget(Main_Screen(name='Main_Screen'))
-        SM.add_widget(Data_base(name='Data_base'))
-        SM.add_widget(Staff_info(name='Staff_info'))
-        return SM
+        self.SM = ScreenManager()
+        self.SM.add_widget(Main_Screen(name='Main_Screen'))
+        self.SM.add_widget(Data_base(name='Data_base'))
+        self.SM.add_widget(Staff_info(name='Staff_info'))
+        return self.SM
+    
+    def set_dark_theme(self):
+        self.theme_cls.theme_style = "Dark"
+
+    def set_light_theme(self):
+        self.theme_cls.theme_style = "Light"
+
+    def go_to_main(self):
+        self.SM.current = 'Main_Screen'
+
 
 
 if __name__ == '__main__':
